ca0a200f02276f87f1b91e8a5142de5f7d9c7702
LeandroCahuana <leandro.cahuana@vallegrande.edu.pe>
2026-03-19 08:54:21 -0500

    feat(structure): Build a Spring Webflux structure

---
+++ src/test/java/ap1/leandro/cahuana/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ap1.leandro.cahuana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
